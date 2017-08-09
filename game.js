fca9eb162b78cad03162f3eec3e580ab74fc0dd0
athdot <athdot@gmail.com>
2017-08-09 17:26:02 -0600

    Update game.js

--- game.js
+++ game.js
@@ -9,6 +9,7 @@ if (window.innerWidth > window.innerHeight) {
     c.height = window.innerWidth;
     var spread = (window.innerWidth-c.width)/2
 }
+console.log(spread);
 c.id = "canvas";
 c.style = "position: absolute; top: 50%; left: 50%; transform:translate(-50%,-50%); background-color: #1f1f1f; box-shadow: 0px 0px 30px " + spread + "px rgb(100,100,100);";
 var ctx = c.getContext("2d");
